b2f22d3176f67d152c479ca87018de02b343d233
Lucas Castro <lucasamonrc@gmail.com>
2022-10-22 18:58:00 -0600

    Handle arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+fn parse_markdown_file(_filename: &str) {
+    print_short_banner();
+    println!("[ INFO ] Trying to parse {}...", _filename)
+}
+
+fn print_short_banner() {
+    println!("{}", get_title());
+}
+
+fn print_long_banner() {
+    print_short_banner();
+    println!(
+        "Written by: {}\nHomepage: {}\nUsage: tinymd <somefile>.md\n",
+        env!("CARGO_PKG_AUTHORS"),
+        env!("CARGO_PKG_HOMEPAGE")
+    );
+}
+
+fn get_title() -> String {
+    let mut the_title = String::from(env!("CARGO_PKG_NAME"));
+    the_title.push_str(" (v");
+    the_title.push_str(env!("CARGO_PKG_VERSION"));
+    the_title.push_str("), ");
+    the_title.push_str(env!("CARGO_PKG_DESCRIPTION"));
+    return the_title;
+}
+
+fn usage() {
+    print_long_banner()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    match args.len() {
+        2 => parse_markdown_file(&args[1]),
+        _ => {
+            println!("[ ERROR ] Invalid invocation");
+            usage();
+        }
+    }
 }
